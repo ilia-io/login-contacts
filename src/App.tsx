234bef62fbf74b7f5bb5383dc5b02186f38daabb
ilia-io <likeapu@gmail.com>
2022-09-27 21:17:58 +0300

    feat: login page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import Login from './pages/Login';
 
-function App() {
-  return (
-    <div className="App">
+const DefaultApp = () => <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -51,6 +47,12 @@ function App() {
           </a>
         </span>
       </header>
+    </div>
+
+function App() {
+  return (
+    <div className='container'>
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Login.module.scss';
+
+const Login = () => {
+  return (
+    <main style={styles}>
+      <h1>Login</h1>
+      <form action="submit">
+        <div>
+          <input type="text" placeholder="Email" id="email" />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" id="password" />
+        </div>
+        <div>
+          <button type="button">Login</button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Login;
