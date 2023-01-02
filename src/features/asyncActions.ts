c610ccfaf1fdbfc6fd405e73694ec38e3dc59b3c
ilia-io <likeapu@gmail.com>
2023-01-02 20:05:47 +0300

    change api service

--- src/features/asyncActions.ts
+++ src/features/asyncActions.ts
@@ -3,13 +3,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { IUser } from '../@types/user';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const fetchUsers = createAsyncThunk(
   'user/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get<IUser[]>(
-        'https://login-contact-server.herokuapp.com/users'
-      );
+      const response = await axios.get<IUser[]>(API_URL!);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue('Не удалось загрузить пользователей');
@@ -21,15 +21,12 @@ export const postUsers = createAsyncThunk(
   'user/post',
   async (userData: IUser, thunkAPI) => {
     try {
-      const response = await axios.post<IUser>(
-        'https://login-contact-server.herokuapp.com/users',
-        {
-          id: userData.id,
-          name: userData.name,
-          email: userData.email,
-          password: userData.password,
-        }
-      );
+      const response = await axios.post<IUser>(API_URL!, {
+        id: userData.id,
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue('Не удалось добавить пользователя');
@@ -41,15 +38,12 @@ export const putUsers = createAsyncThunk(
   'user/put',
   async (userData: IUser, thunkAPI) => {
     try {
-      const response = await axios.put<IUser>(
-        `https://login-contact-server.herokuapp.com/users/${userData.id}`,
-        {
-          id: userData.id,
-          name: userData.name,
-          email: userData.email,
-          password: userData.password,
-        }
-      );
+      const response = await axios.put<IUser>(`${API_URL!}/${userData.id}`, {
+        id: userData.id,
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue('Не удалось обновить данные');
@@ -61,9 +55,7 @@ export const deleteUsers = createAsyncThunk(
   'user/delete',
   async (userData: IUser, thunkAPI) => {
     try {
-      const response = await axios.delete(
-        `https://login-contact-server.herokuapp.com/users/${userData.id}`
-      );
+      const response = await axios.delete(`${API_URL!}/${userData.id}`);
       return userData.id;
     } catch (error) {
       return thunkAPI.rejectWithValue('Не удалось удалить пользователя');
